a37b23e04e8acf13a185a8e4eaa1f27a67a603e5
Sravani Raparthi <sravaniraparthis24@gmail.com>
2026-03-20 21:08:16 +0530

    Created FakerDataGeneratorUtil and also added Created CreateJobAPIFakerDataProvider & the CreateJobAPIFakerDataDriven Test in com.api.test.datadriven package

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -9,39 +9,47 @@ import org.testng.annotations.DataProvider;
 import com.api.request.model.CreateJobPayload;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
+import com.api.utils.FakerDataGenerator;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
 public class DataProviderUtils {
-	
+
 	@DataProvider(name = "LoginAPIDataProvider", parallel = true)
 	public static Iterator<UserBean> loginAPIDataProvider() {
-		
+
 		return CSVReaderUtil.loadCSV("testData/LoginCreds.csv", UserBean.class);
-		
+
 	}
-	
-	//DataProvider needs to return something!! - 3things
-	//[] - 1D Array
-	//[][] - 2D Array
-	//Iterator<>
-	
+
+	// DataProvider needs to return something!! - 3things
+	// [] - 1D Array
+	// [][] - 2D Array
+	// Iterator<>
+
 	@DataProvider(name = "CreateJobAPIDataProvider", parallel = true)
 	public static Iterator<CreateJobPayload> createJobAPIDataProvider() {
-		
-		Iterator<CreateJobBean> createJobBeanIterator = CSVReaderUtil.loadCSV("testData/CreateJobData.csv", CreateJobBean.class);
-		
+
+		Iterator<CreateJobBean> createJobBeanIterator = CSVReaderUtil.loadCSV("testData/CreateJobData.csv",
+				CreateJobBean.class);
+
 		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
-		
+
 		CreateJobBean tempbean;
 		CreateJobPayload tempPayload;
-		
-		while(createJobBeanIterator.hasNext()) {
-			 tempbean = createJobBeanIterator.next();
-			 tempPayload = CreateJobBeanMapper.mapper(tempbean);
-			 payloadList.add(tempPayload);
+
+		while (createJobBeanIterator.hasNext()) {
+			tempbean = createJobBeanIterator.next();
+			tempPayload = CreateJobBeanMapper.mapper(tempbean);
+			payloadList.add(tempPayload);
 		}
-		 return payloadList.iterator();
+		return payloadList.iterator();
+	}
+
+	@DataProvider(name = "CreateJobAPIFakerDataProvider", parallel = true)
+	public static Iterator<CreateJobPayload> CreateJobAPIFakerDataProvider() {
+		Iterator<CreateJobPayload> payloadIterator = FakerDataGenerator.generateFakeCreateJobData(100);
+		return payloadIterator;
 	}
 
 }
