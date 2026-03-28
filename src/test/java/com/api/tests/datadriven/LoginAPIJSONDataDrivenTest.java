2f990b3a576a462126f322a7cb61e4ccdbe3926c
Sravani Raparthi <sravaniraparthis24@gmail.com>
2026-03-29 01:00:00 +0530

    Created the JSON Reader Utility for reading the Login API Test Data.

---
+++ src/test/java/com/api/tests/datadriven/LoginAPIJSONDataDrivenTest.java
@@ -0,0 +1,34 @@
+package com.api.tests.datadriven;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+import com.api.request.model.UserCredentials;
+import com.dataproviders.api.bean.UserBean;
+
+public class LoginAPIJSONDataDrivenTest {
+
+	@Test(description = "Verify if login api is working for FD user", groups = {"api", "regression", "datadriven"}, 
+			dataProviderClass = com.dataproviders.DataProviderUtils.class, dataProvider = "LoginAPIJsonDataProvider" )
+	//After giving dataProviderClass & dataProvider the @Test methods become parameterized, hence we need to pass the parameters
+
+	public void loginAPITest(UserCredentials userCredentials) {
+		//inside a list of dataProviderClass, each one in list is nothing but an ref variable of an UserBean object hence passing ref variable of UserBean type as parameter here
+		
+		given()
+			.spec(requestSpec(userCredentials))
+		.when()
+			.post("login")
+		.then()
+			.spec(responseSpec_OK())
+			.body("message", equalTo("Success"))
+			.and()
+			.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+	}
+
+}
