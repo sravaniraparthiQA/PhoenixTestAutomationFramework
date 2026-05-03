cb3b9eda099da3d97265345ffdc28c2122344403
Sravani Raparthi <sravaniraparthis24@gmail.com>
2026-05-03 18:54:54 +0530

    Introduced MasterService and JobService classes to handle the API Request.
    All Tests Class have been refactored to use the service class.

--- src/test/java/com/api/tests/CreateJobAPIWithDBValidationWithResponseModelTest.java
+++ src/test/java/com/api/tests/CreateJobAPIWithDBValidationWithResponseModelTest.java
@@ -29,6 +29,7 @@ import com.api.request.model.CustomerAddress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
 import com.api.response.model.CreateJobResponseModel;
+import com.api.services.JobService;
 import com.database.dao.CustomerAddressDao;
 import com.database.dao.CustomerDao;
 import com.database.dao.CustomerProductDao;
@@ -40,22 +41,21 @@ import com.database.model.CustomerProductDBModel;
 import com.database.model.JobHeadDBModel;
 import com.database.model.MapJobProblemDBModel;
 
-import io.restassured.response.Response;
-
 public class CreateJobAPIWithDBValidationWithResponseModelTest {
 	
 	private CreateJobPayload createJobPayload;
 	private Customer customer;
 	private CustomerAddress customerAddress;
 	private CustomerProduct customerProduct;
+	private JobService jobService;
 	
-	@BeforeMethod(description = "Creating the create job api request payload")
+	@BeforeMethod(description = "Creating the create job api request payload and instantiating the Job Service")
 	public void setup() {
 		
 		customer = new Customer("Sravani", "Raparthi", "9618096697", "", "sravaniraparthis24@gmail.com", "");
 		customerAddress = new CustomerAddress("Flat 206", "Pushkardham", "Hanuman Nagar", "Morampudi", "Navaram", "533107", "India", "Rajahmundry");
 		
-		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "12775811399044", "12775811399044", "12775811399044", 
+		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "12775811399144", "12775811399144", "12775811399144", 
 				getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
 		
 		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery issue");
@@ -64,16 +64,13 @@ public class CreateJobAPIWithDBValidationWithResponseModelTest {
 		
 		createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), 
 				Warranty_Status.IN_WARRENTY.getCode(), OEM.GOOGLE.getCode(), customer, customerAddress, customerProduct, problemsList);
-		
+		jobService = new JobService();
 	}
 	
 	@Test(description = "Verify if the create job api is able to create Inwarranty job", groups = {"api", "regression", "smoke"})
 	public void createJobAPITest() {
 	
-		CreateJobResponseModel createJobResponseModel = given()
-			.spec(requestSpecWithAuth(Role.FD, createJobPayload))
-		.when()
-			.post("/job/create")
+		CreateJobResponseModel createJobResponseModel = jobService.createJob(Role.FD, createJobPayload)
 		.then()
 			.spec(responseSpec_OK())
 			.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
