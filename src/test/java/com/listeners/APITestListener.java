2d56d8278a4ed40b9b98bc40d86c1e4aa66d6637
Sravani Raparthi <sravaniraparthis24@gmail.com>
2026-06-14 17:28:52 +0530

    Modified the Listener class for revision

--- src/test/java/com/listeners/APITestListener.java
+++ src/test/java/com/listeners/APITestListener.java
@@ -27,7 +27,7 @@ public class APITestListener implements ITestListener{
 		long startTime = result.getStartMillis();
 		long endTime = result.getEndMillis();
 		
-		LOGGER.info("Total Duration:{} ms", (endTime - startTime));
+		LOGGER.info("Total Duration :{} ms", (endTime - startTime));
 		LOGGER.info("{} - Test Passed!!!", result.getName());
 	  }
 	
---
+++ src/test/java/com/listeners/IDemo.java
@@ -0,0 +1,11 @@
+package com.listeners;
+
+public interface IDemo {
+	
+	abstract void m2();
+	abstract void m3();
+	abstract void m4();
+
+	abstract void m1();
+
+}
